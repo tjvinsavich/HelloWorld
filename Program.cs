d684d4752fd18a3ebb553f6657201f112fcb48de
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-09 10:59:04 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name?");
+            var name = Console.ReadLine();
+
+            bool isDefeated = false;
+
+            while (isDefeated == false)
+            {
+                Console.WriteLine($"Now type something funny, {name}.");
+
+                var joke = Console.ReadLine();
+
+                if (joke == "something funny" || joke == $"something funny, {name}" || joke == $"something funny, {name}.")
+                {
+                    Console.WriteLine("...I've been defeated.");
+                    isDefeated = true;
+                }
+                else
+                {
+                    Console.WriteLine("Ha, ha. Very good.");
+                }
+            }
+
+        }
+    }
+}
